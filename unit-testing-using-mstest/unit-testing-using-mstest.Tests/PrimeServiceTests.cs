c4a3c444f82bc14b623d261ce2f3a46879b54308
Caleb Graham <11calebgraham@gmail.com>
2021-02-10 22:31:39 -0600

    Initial commit

---
+++ unit-testing-using-mstest/unit-testing-using-mstest.Tests/PrimeServiceTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using unit_testing_using_mstest;
+
+namespace unit_testing_using_mstest.Tests
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        /*
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+        */
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse(int value)
+        {
+            var primeService = new PrimeService();
+            var result = primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+
+
+
+    }
+}
---
+++ unit-testing-using-mstest/unit-testing-using-mstest/PrimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace unit_testing_using_mstest
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+
+
+    }
+}
